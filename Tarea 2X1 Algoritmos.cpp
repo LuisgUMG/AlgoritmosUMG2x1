dcad3166c160c1cda9b7504aace68c49bfb637af
LuisgUMG <143246181+LuisgUMG@users.noreply.github.com>
2023-08-25 23:33:09 -0600

    Add files via upload

---
+++ Tarea 2X1 Algoritmos.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+
+using namespace std; //Donde se encuentran todas las librerias estandar
+
+main(){
+	
+	/*
+	string nombre
+	cout<<"Hola Mundo..."<<endl;
+	//Variable: es un espacio de memoria reservado para almacenar un valor que corresponde a un tipo de dato.
+
+	//coutc<"Hola mundo... "<<nombre<<endl;
+
+	// tipo nombres de La varicble.
+
+	//C++ case-sensitive: Distinción entre mayúsculas y minúsculas
+
+	// Estilo de nombres para variables: Camel cose
+
+	// para Los clase metodos y funciones LowerComelCase y para Los variables Snake case
+
+	//Irestriccion el nombre de una vorioble no puede : iniciar con numero, no puede tener caracteres especiales, no espacios en blanco     
+
+	string nombreomple_3tos_1= "armando cardona"; // LonerCanelCose
+
+	string nonbreCompleto = "armando cardona"; // LoverComelCase
+
+	string NonbreCompleto = "armando cardona 2"; // UpperComelCase
+
+	string nonbre_completo = "armando cardona 3";// Snake cose
+
+	cout<<nombreCompleto<<endl;
+
+	cout<<NombreCompleto<<endl;
+
+	cout<<nonbre_completo<<endl;
+	
+	
+	system("pause");
+		
+
+	//Variables Numericas
+
+	// Numero enteros
+
+	// como declarar una variable: tipo-nonbres de La variable- valor - ;
+	short entero_corto = 32767; // 2 Bytes (-32,768 a 32,767)
+	int entero=2147083647 ; // 4 Bytes -2,147,483,648 o 2,147,483,647
+	long long entero_largo=12147483648 ; // 2 Eytes
+	unsigned long long i_entero_largo=18446724073709551615 ; // 10 Bytes
+	cout<<"short: "<<entero_corto<<endl;
+	cout<<"int: "<<entero<<endl;
+	cout<<"long: "<<entero_largo<<endl;
+	cout<<"long int : "<<i_entero_largo<<endl;
+	unsigned int int_sin_signo= -2;
+	cout<<"int sin signo: "<<int_sin_signo<<endl;
+	
+	
+	Long double numero decimal = 100.235; // 10 Bytes
+	cout<<"float: "ccnumero decimal<<endl;
+
+	char er='T';
+
+	char asc = 61;
+
+	//logicos 1 o 0, true o false
+	bool logico = true;
+
+	*/
+
+	// Numeros reales de punto flotante
+	
+	
+	char saludo[4];
+
+	saludo[0] = 'H';
+	saludo[1] = '0';
+	saludo[2] = 'L';
+	saludo[3] = 'A';
+
+	cout<<"array: "<<saludo<<endl;
+	cout<<"array : "<<saludo[0]<<saludo[1]<<saludo[2]<<saludo[3]<<endl;
+
+	int codigo[3];
+	codigo[0] = 1280;
+	codigo[1] = 3800;
+	codigo[2] = 1245128;
+	cout<<"codigos:"<<codigo[0]<<","<<codigo[1]<<","<<codigo[2]<<endl;
+
+	string paises[3];
+	paises[0] = "Guatemala";
+	paises[1] = "El Salvador";
+	paises[2] = "Honduras";
+
+	cout<<"codigos:"<<paises[0]<<","<<paises[1]<<","<<paises[2]<<endl;
+	system("pause");	
+}
+
+
+
+
+
+
+
+
+}
